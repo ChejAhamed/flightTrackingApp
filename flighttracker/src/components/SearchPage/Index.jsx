5a93834d1bc23372ea0a0ac846ffe2b9d88f4a1a
Chej Ahamed <74456334+ChejAhamed@users.noreply.github.com>
2022-05-05 12:15:07 +0100

    Add inputs

--- flighttracker/src/components/SearchPage/Index.jsx
+++ flighttracker/src/components/SearchPage/Index.jsx
@@ -1,16 +1,49 @@
-/* eslint-disable react/jsx-no-bind */
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useState } from 'react';
 import { DatePicker, InputNumber, Select } from 'antd';
 
 function Index() {
   const { Option } = Select;
-  function onFlightNumbChange(value) {
+  const [data, setData] = useState({});
+  const [date, setDate] = useState('');
+  const [carrier, setCarrier] = useState('');
+  const [flightNumber, setFlightNumber] = useState('');
+
+  const onFlightNumbChange = (value) => {
+    setFlightNumber(value);
     console.log('change to', value);
-  }
-  function onCarrierChange(value) {
+  };
+  const onCarrierChange = (value) => {
+    setCarrier(value);
     console.log(value);
-  }
+  };
+  const onChangeDate = (date, dateString) => {
+    setDate(dateString);
+    console.log(dateString);
+  };
+  const search = () => {
+    if (!carrier || !flightNumber || !date) {
+      alert('Carrier, flight Number and is required');
+      return;
+    }
+    const firstFormat = date.replace('-', '/');
+    const formattedDate = firstFormat.replace('-', '/');
+    console.log(date, formattedDate);
+    fetch(
+      ` https://cors-anywhere.herokuapp.com/https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/status/${carrier}/${flightNumber}/dep/${formattedDate}`,
+      {
+        method: 'GET',
+        headers: {
+          appID: 'e2116426',
+          appKey: 'a47153b4eab7efc06ff7483243da3999',
+        },
+      },
+    ).then((data) => data.json())
+      .then((finalData) => {
+        setData(finalData);
+        console.log(finalData);
+      });
+  };
   return (
     <div>
       Index
@@ -30,10 +63,35 @@ function Index() {
           </Select>
         </div>
         <div className="FlightNumber"><InputNumber onChange={onFlightNumbChange} placeholder="ex 245,311" /></div>
-        <div className="DepartureDate"><DatePicker /></div>
+        <div className="DepartureDate"><DatePicker onChange={onChangeDate} /></div>
+        <button type="button" onClick={search}>Search</button>
       </div>
+      {data.flightSatutes && (
+      <div className="statuees">
+        <h1 className="searcResult">Search Result</h1>
+        <h1>{data.flightSatutes[0].status}</h1>
+      </div>
+      )}
     </div>
   );
 }
 
 export default Index;
+
+/*
+  const search = () => {
+    fetch(
+      ` https://cors-anywhere.herokuapp.com/https://api.flightstats.com/flex/flightstatus//v2/json/flight/status/${carrier}/${flight}/arr/${year}/${month}/${day}`,
+      {
+        method: 'GET',
+        headers: {
+          appID: 'e2116426',
+          appKey: 'a47153b4eab7efc06ff7483243da3999',
+        },
+      },
+    ).then((data) => data.json())
+      .then((finalData) => {
+        console.log(finalData);
+      });
+  };
+  */
